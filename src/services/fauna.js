3076abd020cd495ac237749bf11b850ad96a9789
EricEOL <lopes.eric051@gmail.com>
2021-04-12 22:03:38 -0300

    Correção de erro

--- src/services/fauna.js
+++ src/services/fauna.js
@@ -1,5 +1,5 @@
 import { Client } from 'faunadb';
 
 export const fauna = new Client({
-    secret: 'fnAEGpMe6yACA6Fgg0IdCiztGm7Ymsi1Jds7TiCr'
+    secret: process.env.fnAEGpMe6yACA6Fgg0IdCiztGm7Ymsi1Jds7TiCr
 })
