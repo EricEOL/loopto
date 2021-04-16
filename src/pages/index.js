f1f010a4f81a55a793b576c75f289f8981173201
ERIC OLIVEIRA LOPES <TEN_LOPES@dcmun.eb.mil>
2021-04-16 06:19:05 +0530

    Inserção de função de reload de preços

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,45 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
 import { LineGraphic } from '../components/LineGraphic';
 import moment from 'moment';
 import styles from './home.module.scss';
 
-export default function Home({ ripple, tether, rippleDates, ripplePrices, tetherDates, tetherPrices }) {
+export default function Home({ rippleDates, ripplePrices, chainlinkDates, chainlinkPrices }) {
+
+  const [ripple, setRipple] = useState([]);
+  const [chainlink, setChainlink] = useState([]);
+  const [reloadData, setReloadData] = useState(0);
+
+  useEffect( async () => {
+    const rippleData = await fetch('https://api.coingecko.com/api/v3/coins/ripple?localization=false&community_data=false&developer_data=false&sparkline=false')
+      .then(response => response.json())
+      .then(data => data);
+
+    const rippleFormated = {
+      brl_price: parseFloat(rippleData.market_data.current_price.brl.toFixed(3)),
+      usd_price: parseFloat(rippleData.market_data.current_price.usd.toFixed(3)),
+      last_24hours: parseFloat(rippleData.market_data.price_change_percentage_24h.toFixed(2)),
+      image: rippleData.image.small
+    }
+    setRipple(rippleFormated);
+
+    const chainlinkData = await fetch('https://api.coingecko.com/api/v3/coins/chainlink?localization=false&community_data=false&developer_data=false&sparkline=false')
+      .then(response => response.json())
+      .then(data => data);
+
+    const chainlinkFormated = {
+      brl_price: parseFloat(chainlinkData.market_data.current_price.brl.toFixed(3)),
+      usd_price: parseFloat(chainlinkData.market_data.current_price.usd.toFixed(3)),
+      last_24hours: parseFloat(chainlinkData.market_data.price_change_percentage_24h.toFixed(2)),
+      image: chainlinkData.image.small
+    }
+    setChainlink(chainlinkFormated);
+
+  }, [reloadData]);
+
+  setInterval(()=>{
+    setReloadData(reloadData + 1);
+  }, 10000)
 
   return (
     <>
@@ -24,13 +60,13 @@ export default function Home({ ripple, tether, rippleDates, ripplePrices, tether
         </div>
         <div className={styles.card}>
           <div className={styles.cardData}>
-            <img src={tether.image} alt="tether" />
-            <h2>Tether</h2>
-            <span>BRL: <strong>{tether.brl_price}</strong></span>
-            <span>USD: <strong>{tether.usd_price}</strong></span>
-            <span>24h: <strong className={tether.last_24hours > 0 ? styles.up : styles.down}>{tether.last_24hours}%</strong></span>
+            <img src={chainlink.image} alt="chainlink" />
+            <h2>Chainlink</h2>
+            <span>BRL: <strong>{chainlink.brl_price}</strong></span>
+            <span>USD: <strong>{chainlink.usd_price}</strong></span>
+            <span>24h: <strong className={chainlink.last_24hours > 0 ? styles.up : styles.down}>{chainlink.last_24hours}%</strong></span>
           </div>
-          <LineGraphic data={tetherPrices} labels={tetherDates} title="tether" />
+          <LineGraphic data={chainlinkPrices} labels={chainlinkDates} title="chainlink" />
         </div>
       </main>
 
@@ -42,28 +78,6 @@ export const getServerSideProps = async () => {
 
   const timestampToday = Date.now();
   const timestamp3monthsAgo = moment(timestampToday).subtract('months', 3).unix();
-
-  const rippleData = await fetch('https://api.coingecko.com/api/v3/coins/ripple?localization=false&community_data=false&developer_data=false&sparkline=false')
-    .then(response => response.json())
-    .then(data => data);
-
-  const ripple = {
-    brl_price: parseFloat(rippleData.market_data.current_price.brl.toFixed(3)),
-    usd_price: parseFloat(rippleData.market_data.current_price.usd.toFixed(3)),
-    last_24hours: parseFloat(rippleData.market_data.price_change_percentage_24h.toFixed(2)),
-    image: rippleData.image.small
-  }
-
-  const tetherData = await fetch('https://api.coingecko.com/api/v3/coins/tether?localization=false&community_data=false&developer_data=false&sparkline=false')
-    .then(response => response.json())
-    .then(data => data);
-
-  const tether = {
-    brl_price: parseFloat(tetherData.market_data.current_price.brl.toFixed(3)),
-    usd_price: parseFloat(tetherData.market_data.current_price.usd.toFixed(3)),
-    last_24hours: parseFloat(tetherData.market_data.price_change_percentage_24h.toFixed(2)),
-    image: tetherData.image.small
-  }
 
   let rippleDates = [];
   let ripplePrices = [];
@@ -85,33 +99,31 @@ export const getServerSideProps = async () => {
     ripplePrices.push(price);
   })
 
-  let tetherDates = [];
-  let tetherPrices = [];
+  let chainlinkDates = [];
+  let chainlinkPrices = [];
 
-  const tetherPriceRange = await fetch(`https://api.coingecko.com/api/v3/coins/tether/market_chart/range?vs_currency=usd&from=${timestamp3monthsAgo}&to=${timestampToday}`)
+  const chainlinkPriceRange = await fetch(`https://api.coingecko.com/api/v3/coins/chainlink/market_chart/range?vs_currency=usd&from=${timestamp3monthsAgo}&to=${timestampToday}`)
     .then(response => response.json())
     .then(data => data);
 
-  const tetherDataPrices = tetherPriceRange.prices;
-  const filtertetherPrices = tetherDataPrices.filter((price, index) => index >= pricesLength - 20);
+  const chainlinkDataPrices = chainlinkPriceRange.prices;
+  const filterchainlinkPrices = chainlinkDataPrices.filter((price, index) => index >= pricesLength - 20);
 
-  filtertetherPrices.map(item => {
+  filterchainlinkPrices.map(item => {
 
     const date = moment(item[0]).format('DD/MM/YYYY');
     const price = parseFloat(item[1]).toFixed(2);
 
-    tetherDates.push(date);
-    tetherPrices.push(price);
+    chainlinkDates.push(date);
+    chainlinkPrices.push(price);
   })
 
   return {
     props: {
-      ripple,
-      tether,
       rippleDates,
       ripplePrices,
-      tetherDates,
-      tetherPrices
+      chainlinkDates,
+      chainlinkPrices
     }
   }
 }
